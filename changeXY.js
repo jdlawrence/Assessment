271951f5367b92428d4c31ba4123879788bb979a
jdlawrence <jamil.lawrence@gmail.com>
2017-01-23 10:28:19 -0800

    Init

---
+++ changeXY.js
@@ -0,0 +1,15 @@
+// Given a string, compute recursively (no loops) a new string 
+//where all the lowercase 'x' chars have been changed to 'y' chars.
+
+
+/* Examples 
+changeXY('codex') → 'codey'
+changeXY('xxhixx') → 'yyhiyy'
+changeXY('xhixhix') → 'yhiyhiy'
+*/
+
+var _ = {};
+
+_.changeXY = function(input) {
+  // your code here!
+};
---
+++ groupBy.js
@@ -0,0 +1,19 @@
+// groupBy:
+// Splits a collection into sets, grouped by the result of running each value through 
+// iteratee. If iteratee is a string instead of a function, groups by the property 
+// named by iteratee on each of the values.
+
+// Examples:
+
+// _.groupBy([1.3, 2.1, 2.4], function(num){ return Math.floor(num); });
+// // returns {1: [1.3], 2: [2.1, 2.4]}
+
+// _.groupBy(['one', 'two', 'three'], 'length');
+// // returns {3: ["one", "two"], 5: ["three"]}
+
+var _ = {};
+
+_.groupBy = function(collection, iteratorOrstring) {
+  // your code here!
+};
+
